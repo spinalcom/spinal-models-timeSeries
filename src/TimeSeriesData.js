e40ddcba6088b12229ccfd2bc13df3fa7680fe86
Moussa <mbaradji221@outlook.fr>
2018-10-26 11:37:08 +0200

    build

--- src/TimeSeriesData.js
+++ src/TimeSeriesData.js
@@ -2,7 +2,7 @@ const spinalCore = require("spinal-core-connectorjs");
 const globalType = typeof window === "undefined" ? global : window;
 import {
   Utilities
-} from "../Utilities";
+} from "./Utilities";
 
 /**
  *
